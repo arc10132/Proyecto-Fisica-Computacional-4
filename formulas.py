ce283eec4ba99681f58f2eda936ac9e08c4da4e9
tomas inzulza <tomas.inzulza@usach.cl>
2026-01-25 15:45:26 -0300

    se actualizan los archivos Discos_protoplanetarios.ipynb y formulas.py

--- formulas.py
+++ formulas.py
@@ -5,6 +5,8 @@ Created on 2026-01-23
 # Importamos las librerias necesarias para los calculos dentro del modulo
 
 import numpy as np
+import scipy.constants as consts
+
 
 # Definimos la funcion para calcular el perfil de densidad del disco protoplanetario
 def density_profile(r, rho_0, r_0, p, r_min):
@@ -23,3 +25,35 @@ def density_profile(r, rho_0, r_0, p, r_min):
     density = np.where(r >= r_min, rho_0 * (r / r_0) ** (-p), 0)
 
     return density
+
+def star_density(x , y , Mass, Radius):
+    """"
+    Calcula el perfil de densidad de una estrella.
+    Args:
+        x (Array):  Eje x de la malla
+        y (Array):  Eje y de la malla      
+        Mass (Float): Masa de la estrella, idealmente con unidades de kg      
+        Radius (Floar): Radio de la estrella, idealmente con unidades de au     
+
+    Returns:
+        Density(Array(): Densidad superficial de la estrella en cada punto de la malla, condicionada al tamaño del radio 
+    """
+    x=x
+    y=y
+    r= np.sqrt(x**2 + y**2)
+    star_density = np.where(r <= Radius, Mass / (np.pi * Radius**2) , 0 )
+
+    return star_density
+
+
+def Poisson_equation(x , y ,dx, star_density, density_profile):
+    """
+    """
+    phi = np.zeros_like(x)
+
+    for i in range(1, x.shape[0]-1):
+        for j in range(1, y.shape[1]-1):
+            phi[i,j]= ((phi[i,j+1]+phi[i+1,j]+phi[i,j-1]+phi[i-1,j])/4)- np.pi* consts.G * (star_density[i,j] +density_profile[i,j] *dx**2)
+            
+    return phi 
+          
